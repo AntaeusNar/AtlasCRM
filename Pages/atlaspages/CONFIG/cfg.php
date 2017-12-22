761f72e3a981e024ad076268ff81696065755216
AntaeusNar <jedidiah.h.wallace@gmail.com>
2017-12-22 10:08:14 -0800

    Added files to start the seperation of personal and atlas CRM projects

---
+++ Pages/atlaspages/CONFIG/cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//menu array
+$mainmenu = array(
+	'home' => array('text'=>'Home', 'url'=>'?p=home'),
+	'timetracker' => array('text'=>'Time Tracker', 'url'=>'?p=timetracker'),
+	'about'=> array('text'=>'About', 'url'=>'?p=about'),
+);
+
+?>
