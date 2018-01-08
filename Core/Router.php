8e6c87aabd285ed042ed75ae581c858084a27b1f
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-08 14:37:35 -0800

    more of the same

--- Core/Router.php
+++ Core/Router.php
@@ -1,7 +1,16 @@
 <?php
 class Router {
 	
+	protected $routes = [];
 	
+	public Function add($route, $params) {
+		$this->routes[$route] = $parms;
+	}
+	
+	public Function getRoutes(){
+		return $this->routes;
+	}
+
 	
 	
 }
