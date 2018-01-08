082dfb704ce85d46c37f4123ddf1df7b613664d9
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-08 14:57:23 -0800

    added matching and params returning fuctions to router

--- Core/Router.php
+++ Core/Router.php
@@ -2,17 +2,34 @@
 class Router {
 	
 	protected $routes = [];
+	protected $params = [];
 	
+	//adds a route to the route array with passed params
 	public Function add($route, $params) {
 		$this->routes[$route] = $params;
 	}
 	
+	//returns all routes
 	public Function getRoutes(){
 		return $this->routes;
 	}
-
+	
+	//matchs the given url to a route if exsits give true, else gives false
+	public function match($url){
+		foreach ($this->routes as $route => $params) {
+			if($url == $route) {
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
+	//returns the current matched parameters as an array
+	public function getParams(){
+		return $this->params;
+	}
 }
 
 ?>
