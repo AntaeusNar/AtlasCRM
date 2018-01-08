06cec8dec7f82d19faa5db7097e189581489fbeb
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-08 15:28:55 -0800

    added reg exp to capture the url

--- Core/Router.php
+++ Core/Router.php
@@ -4,6 +4,8 @@ class Router {
 	protected $routes = [];
 	protected $params = [];
 	
+
+
 	//adds a route to the route array with passed params
 	public Function add($route, $params) {
 		$this->routes[$route] = $params;
@@ -16,12 +18,34 @@ class Router {
 	
 	//matchs the given url to a route if exsits give true, else gives false
 	public function match($url){
+		
+		//Match to fixed URL format /controller/action
+		$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+		
+		if (preg_match($reg_exp, $url, $matches)) {
+			//get named capture groups
+			$params = [];
+			
+			foreach ($matches as $key =>$match) {
+				if (is_string($key)) {
+					$params[$key] = $match;
+				}
+			}
+			
+			$this->params = $params;
+			return true;
+			
+		}
+		
+		/*old
 		foreach ($this->routes as $route => $params) {
 			if($url == $route) {
 				$this->params = $params;
 				return true;
 			}
 		}
+		*/
+		
 		return false;
 	}
 	
