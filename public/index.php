a9ba29c612fdc4936b8adea7b8b466b7b2d2c577
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-08 20:20:45 -0800

    more work on the router and home.php

--- public/index.php
+++ public/index.php
@@ -2,44 +2,19 @@
 //starting the front controller//
 
 //Requires
-
+require '../App/Controllers/Home.php';
 require '../Core/Router.php';
 
 
 $router = new Router();
 
-/* some debug
-echo "Class: " .get_class($router). " Loaded <br>";
-
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
-*/
 
 //Adding the routes multidimentional array with each route having a name, then a controller and finally an action
 $router->add('',['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
-
-//Display the routing table
-echo '<pre>';
-//var_dump($router->getRoutes());
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
-
-
-//Matching the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if($router->match($url)) {
-	echo '<pre>';
-	var_dump($router->getParams());
-	echo '</pre>';
-} else {
-	echo "No route found for URL '$url'";
-}
-
+$router->dispatch($_SERVER['QUERY_STRING']);
 
 
 /* old code
