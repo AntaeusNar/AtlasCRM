2549916c17a083490619cdf84739443e104c26ff
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-08 16:24:11 -0800

    fixed flipped placement of controller and action

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $router->add('',['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 //$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
-$router->add('admin/{action}/{controller}');
+$router->add('admin/{controller}/{action}');
 
 //Display the routing table
 echo '<pre>';
