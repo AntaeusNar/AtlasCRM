94c1faf544e39a09c03709e8251178d4e4ca5683
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-11 15:02:13 -0800

    changed wording in debug

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -8,7 +8,7 @@ class Home extends \Core\Controller
 	public function index()
 	{
 		echo "hello from home!";
-		echo '<p>Query string parameters from a get: <pre>' . htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
+		echo '<p>Query string parameters from route_params: <pre>' . htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
 	}
 }
 
