8112a0301da71c739691d2ae4a7e0b90c4077310
AntaeusNar <jedidiah.h.wallace@gmail.com>
2018-01-14 08:45:19 -0800

    adding ../ to href links seems to fix the linking problems

--- App/Config.php
+++ App/Config.php
@@ -11,9 +11,9 @@ class Config
 	
 	//menu array
 	private static $MainMenuConfig = array(
-		'home' => array('text'=>'Home', 'url'=>'atlas.local/home/index'),
-		'timetracker' => array('text'=>'Time Tracker', 'url'=>'AisTime/index'),
-		'about'=> array('text'=>'About', 'url'=>'about/index'),
+		'home' => array('text'=>'Home', 'url'=>'../home/index'),
+		'timetracker' => array('text'=>'Time Tracker', 'url'=>'../AisTime/index'),
+		'about'=> array('text'=>'About', 'url'=>'../about/index'),
 	);
 	
 	//returns the mainmenu array
